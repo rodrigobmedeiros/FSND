6dc7dcabd08db8f17585e986a2df1c29c9514184
rodrigobmedeiros <rodrimedeiros@gmail.com>
2021-03-08 07:39:13 -0300

    Include test for /categories endpoint

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,19 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.user = "postgres"
+        self.password = "draGao01"
+        self.database_path = ''.join(['postgres', 
+                                      '://',
+                                      self.user,
+                                      ':',
+                                      self.password,
+                                      '@',
+                                      'localhost:5432',
+                                      '/',
+                                      self.database_name])
+
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +45,17 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories_success(self):
+        """
+        Test success of endpoint /categories.
+        """
+        res = self.client().get('/categories')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.get_json()['success'], True)
+
+    def test_get_question_success(self):
+        pass
+
 
 
 # Make the tests conveniently executable
